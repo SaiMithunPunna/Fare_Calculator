dc3d4e1d3f2b253ffbe304183709ef768dd77a76
SaiMithunPunna <saimithunamangal@gmail.com>
2026-04-03 20:42:43 +0530

    fix : validate inputs

--- fare_calc.py
+++ fare_calc.py
@@ -1,47 +1,52 @@
-vehicle_price={'Economy':10, 'Premium': 18,'SUV': 25}
+vehicle_price={'economy':10, 'premium': 18,'suv': 25}
 
+print("\n           CityCab Ride Estimate      \n")
 #user inputs
-vehicle_type=(input("Enter the type of vehicle (Economy , Premium , SUV ): "))
-distance=float(input("Enter the distance to travel (in km) : "))
-hour=int(input("Enter the hour of the day(0-23) : "))
+try:
+    vehicle_type=(input("Enter the type of vehicle (Economy , Premium , SUV ): "))
+    distance=float(input("Enter the distance to travel (in km) : "))
+    hour=int(input("Enter the hour of the day(0-23) : "))
 
 
-#calculate fare function
-def calculate_fare(km , type , hour ):
+    #calculate fare function
+    def calculate_fare(km , type , hour ):
 
-    #error handling
-    try:
-        fare=km*vehicle_price[type]
+        #error handling
+        try:
+            
+            fare=km*vehicle_price[type.lower()]
 
-        #surge pricing 
-        surge_applied=False
-        if(hour>=17 and hour<=20):
-            surge_applied=True
-            fare=surge_pricing(fare)
+            #surge pricing 
+            surge_applied=False
+            if(hour>=17 and hour<=20):
+                surge_applied=True
+                fare=surge_pricing(fare)
+            
+            #print receipt
+            print_receipt(type , km ,hour , fare , surge_applied )
+            
+        except:
+            print("Service Not Available")
+            
         
-        #print receipt
-        print_receipt(type , km ,hour , fare , surge_applied )
-        
-    except:
-        print("Service Not Available")
-        
-    
-def surge_pricing(fare):
-    return fare*1.5
+    def surge_pricing(fare):
+        return fare*1.5
 
-def print_receipt(type , km ,hour , fare , surge_applied):
-    print("\n\n             Fare receipt")
-    print("-------------------------------------")
-    print("     Vehicle type :   ", vehicle_type)
-    print("     Distance     :   " , km , "km")
-    print("     Hour         :   ", hour)
-    
-    if(surge_applied):
-        print("     Surge        :    Applied(1.5x)")
-    else:
-        print("     Surge        :    Not applied")
-    print("-------------------------------------")
-    print(f"     Total Fare   :    ₹{fare:.2f}"  )
-    print("-------------------------------------\n\n")
+    def print_receipt(type , km ,hour , fare , surge_applied):
+        print("\n\n             Fare receipt")
+        print("-------------------------------------")
+        print("     Vehicle type :   ", vehicle_type)
+        print("     Distance     :   " , km , "km")
+        print("     Hour         :   ", hour)
+        
+        if(surge_applied):
+            print("     Surge        :    Applied(1.5x)")
+        else:
+            print("     Surge        :    Not applied")
+        print("-------------------------------------")
+        print(f"     Total Fare   :    ₹{fare:.2f}"  )
+        print("-------------------------------------\n\n")
 
-calculate_fare(distance , vehicle_type , hour)
+    calculate_fare(distance , vehicle_type , hour)
+except:
+    print("Please provide valid inputs")
