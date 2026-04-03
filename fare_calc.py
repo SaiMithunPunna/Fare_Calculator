c5c2abcd7c2564e492bc28c875fd357d38be8bde
SaiMithunPunna <saimithunamangal@gmail.com>
2026-04-03 20:09:46 +0530

    add error handling

--- fare_calc.py
+++ fare_calc.py
@@ -1,14 +1,23 @@
 vehicle_price={'Economy':10, 'Premium': 18,'SUV': 25}
-hour=int(input("Enter the hour of the day(0-23) : "))
+
 vehicle_type=(input("Enter the type of vehicle (Economy , Premium , SUV ): "))
 distance=int(input("Enter the distance to travel (in km) : "))
+hour=int(input("Enter the hour of the day(0-23) : "))
 
 #calculate fair function
 def calculate_fair(km , type , hour ):
-    fair=km*vehicle_price[type]
-    #surge pricing 
-    if(hour>=17 and hour<=20):
-        fair=surge_pricing(fair)
-    return fair 
+    try:
+        fair=km*vehicle_price[type]
+        #surge pricing 
+        if(hour>=17 and hour<=20):
+            fair=surge_pricing(fair)
+        
+    except:
+        print("Service Not Available")
+        
+    
 def surge_pricing(fair):
     return fair*1.5
+
+
+#Error handling
