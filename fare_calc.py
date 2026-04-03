f45419a170e9e61f3a6e6af8bc750c6413f1ff04
SaiMithunPunna <saimithunamangal@gmail.com>
2026-04-03 19:55:04 +0530

    add calculate_fair function

--- fare_calc.py
+++ fare_calc.py
@@ -1,3 +1,9 @@
 vehicle_price={'Economy':10, 'Premium': 18,'SUV': 25}
-hour=int(input("Enter the hour"))
-vehicle_type=(input("Enter the type of vehicle:"))
+hour=int(input("Enter the hour of the day(0-23) : "))
+vehicle_type=(input("Enter the type of vehicle : "))
+distance=int(input("Enter the distance to travel (in km) : "))
+
+#calculate fair function
+def calculate_fair(km , type , hour ):
+    fair=km*vehicle_price[type]
+    return fair 
